9e752d30226d925bfb266b89afaf3dc2f442e6f8
Damian Suidgeest <damiansuidgeest@gmail.com>
2020-02-22 15:33:02 -0700

    Upload stuff

--- src/main/java/frc/robot/subsystems/Climb.java
+++ src/main/java/frc/robot/subsystems/Climb.java
@@ -1,5 +1,6 @@
 package frc.robot.subsystems;
 
+import com.revrobotics.CANEncoder;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
@@ -7,6 +8,8 @@ import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.VictorSP;
 import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.utilities.MiscUtils;
+import frc.robot.utilities.PID;
 
 public class Climb extends SubsystemBase {
 
@@ -14,22 +17,42 @@ public class Climb extends SubsystemBase {
         UP, DOWN, OFF;
     }
 
+    private final double kP = 0;
+    private final double kI = 0;
+    private final double kD = 0;
+
+    private PID holdExtenderArm;
+
     private VictorSP smallWinch;
     private CANSparkMax bigWinch;
-
+    private CANEncoder bigWinchEncoder;
     private DoubleSolenoid solenoid;
 
     public Climb() {
+        holdExtenderArm = new PID(kP, kI, kD, 0);
+
         smallWinch = new VictorSP(7000); // lol
         bigWinch = new CANSparkMax(7, MotorType.kBrushless);
-
-        //                            forwardChannel, reverseChannel
+        bigWinchEncoder = new CANEncoder(bigWinch);
+        //               forwardChannel, reverseChannel
         solenoid = new DoubleSolenoid(0, 0);
     }
 
     @Override
     public void periodic() {
 
+    }
+
+    public double getWinchEncoder() {
+        return MiscUtils.encoderToSpeed(bigWinchEncoder.getCountsPerRevolution(), bigWinchEncoder.getVelocity());
+    }
+
+    public void setSmallWinchSpeed(double speed) {
+        smallWinch.set(speed);
+    }
+
+    public void setSmallWinchHold() {
+        smallWinch.set(holdExtenderArm.getOutput(1));
     }
 
     public void setExtenderArmState(ExtenderArmStates state) {
@@ -45,6 +68,4 @@ public class Climb extends SubsystemBase {
                 break;
         }
     }
-
-
 }
--- src/main/java/frc/robot/subsystems/Indexing.java
+++ src/main/java/frc/robot/subsystems/Indexing.java
@@ -1,42 +1,35 @@
 package frc.robot.subsystems;
 
-import com.revrobotics.CANEncoder;
-import com.revrobotics.CANSparkMax;
-import com.revrobotics.CANSparkMaxLowLevel.MotorType;
-
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.utilities.MiscUtils;
 import frc.robot.utilities.PID;
 
-public class Shooter extends SubsystemBase {
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+
+public class Indexing extends SubsystemBase {
+
+    private TalonSRX leftMotor;
+    private TalonSRX rightMotor;
+    // need encoders too
 
     private boolean spinning;
-    private PID shooterPID;
-
-    private CANSparkMax leftMotor;
-    private CANSparkMax rightMotor;
-    private CANEncoder leftEncoder;
-    private CANEncoder rightEncoder;
+    private PID indexingPID;
 
     private final double kP = 0;
     private final double kI = 0;
     private final double kD = 0;
-    private final double target = 6000; // RPM
+    private final double target = 400; // RPM
 
-    public Shooter() {
-        spinning = false;
-        shooterPID = new PID(kP, kI, kD, target);
+    public Indexing() {
+        leftMotor = new TalonSRX(10);
+        leftMotor = new TalonSRX(11);
 
-        leftMotor = new CANSparkMax(5, MotorType.kBrushless);
-        rightMotor = new CANSparkMax(6, MotorType.kBrushless);
-        leftEncoder = new CANEncoder(leftMotor);
-        rightEncoder = new CANEncoder(rightMotor);
+        indexingPID = new PID(kP, kI, kD, target);
     }
 
     @Override
     public void periodic() {
         if (spinning) {
-            // Not sure if I have to use the miscutils function with the spark maxes, test this
+            // Not sure if I have to use the miscutils function with the encoders, test this
             leftMotor.set(MiscUtils.encoderToSpeed(leftEncoder.getCountsPerRevolution(), shooterPID.getOutput(leftEncoder.getVelocity())));
             rightMotor.set(-MiscUtils.encoderToSpeed(rightEncoder.getCountsPerRevolution(), shooterPID.getOutput(rightEncoder.getVelocity())));
         } else {
@@ -44,6 +37,7 @@ public class Shooter extends SubsystemBase {
             rightMotor.set(0);
         }
     }
+
 
     /**
      * These methods return true or false as a check to see if the shooter spins up when we don't want it to
