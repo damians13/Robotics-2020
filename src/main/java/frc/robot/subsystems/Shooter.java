77f6a003087927e0a578ba4138e126cc399df227
Rundle Shared ThinkPad <srhigh@rundle.ab.ca>
2020-02-24 17:45:39 -0700

    Update Shooter.java

--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -6,7 +6,6 @@ import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.utilities.MiscUtils;
 import frc.robot.utilities.PID;
 
 public class Shooter extends SubsystemBase {
