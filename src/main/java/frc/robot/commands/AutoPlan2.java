efb18c291e56d78ae64469e9c5aa324ba41a00b7
Rundle Shared ThinkPad <srhigh@rundle.ab.ca>
2020-03-01 14:14:12 -0700

    Updates again

--- src/main/java/frc/robot/commands/AutoPlan2.java
+++ src/main/java/frc/robot/commands/AutoPlan2.java
@@ -28,18 +28,18 @@ public class AutoPlan2 extends CommandGroupBase {
  */
 
     public AutoPlan2() {
-        sequence(new DoNothing(50));
-        parallel(new TogglePneumatic(Solenoids.CLIMB),
-                 new TogglePneumatic(Solenoids.INTAKE));
-        sequence(new SetWheelSpeed(-0.3, 0, 0));
-        sequence(new DoNothing(50));
-        sequence(new SetWheelSpeed(0, 0, 0));
-        sequence(new AutoAimLimelightSimp());
-        sequence(new AutoSpin(Spinnables.SHOOTER));
-        sequence(new DoNothing(50));
-        sequence(new AutoSpin(Spinnables.INDEXING));
-        sequence(new DoNothing(150));
-        parallel(new AutoSpin(Spinnables.SHOOTER),
+        sequence(new DoNothing(50),
+                 new TogglePneumatic(Solenoids.CLIMB),
+                 new TogglePneumatic(Solenoids.INTAKE),
+                 new SetWheelSpeed(-0.3, 0, 0),
+                 new DoNothing(50),
+                 new SetWheelSpeed(0, 0, 0),
+                 new AutoAimLimelightSimp(),
+                 new AutoSpin(Spinnables.SHOOTER),
+                 new DoNothing(50),
+                 new AutoSpin(Spinnables.INDEXING),
+                 new DoNothing(150),
+                 new AutoSpin(Spinnables.SHOOTER),
                  new AutoSpin(Spinnables.INDEXING));
     }
 
